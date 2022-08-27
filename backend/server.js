66794168970d05276ef6ac1c9250dcdf9ee9de7b
kiburtemesgen <kiburezelast@gmail.com>
2022-08-27 18:35:21 +0300

    heroku remote

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+console.log(process.env.NODE_ENV);
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
